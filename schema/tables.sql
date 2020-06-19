d4916f8ec712746bf06c33f763cde26e38de4f2a
hupeng <bitrocks.hu@gmail.com>
2020-06-19 19:31:09 +0800

    feat: update sql schema

--- schema/tables.sql
+++ schema/tables.sql
@@ -1,45 +1,35 @@
-CREATE TABLE IF NOT EXISTS blocks(
-    id SERIAL NOT NULL,
-    number numeric NOT NULL,
-    hash bytea NOT NULL,
+CREATE TABLE IF NOT EXISTS block_digests(
+    id SERIAL PRIMARY KEY,
+    block_hash bytea UNIQUE NOT NULL,
+    block_number NUMERIC NOT NULL,
     parent_hash bytea NOT NULL,
-
-    is_orphan boolean DEFAULT false,
+    -- is_orphan boolean DEFAULT false,
 );
-
-CREATE TABLE IF NOT EXISTS transactions(
-    id SERIAL NOT NULL,
-    hash bytea NOT NULL,
-
-    block_id INTEGER REFERENCES blocks(id),
+CREATE TABLE IF NOT EXISTS transaction_digests(
+    id SERIAL PRIMARY KEY,
+    tx_hash bytea UNIQUE NOT NULL,
+    block_id INTEGER REFERENCES block_digests(id),
 );
-
-CREATE TABLE IF NOT EXISTS cell_outputs(
-    id SERIAL NOT NULL,
-    capacity numeric NOT NULL,
-    lock_script_hash bytea NOT NULL,
-    type_script_hash bytea,
-
-    is_live boolean NOT NULL DEFAULT true,
-
-    tx_id INTEGER REFERENCES transactions(id),
+CREATE TABLE IF NOT EXISTS cells(
+    id SERIAL PRIMARY KEY,
+    capacity NUMERIC NOT NULL,
+    lock_script_id INTEGER REFERENCES scripts(id) NOT NULL,
+    type_script_id INTEGER REFERENCES scripts(id),
+    consumed boolean NOT NULL DEFAULT true,
+    tx_id INTEGER REFERENCES transaction_digests(id),
     tx_hash bytea NOT NULL,
-    output_index
+    index INTEGER NOT NULL,
 );
-
-CREATE TABLE IF NOT EXISTS cell_inputs(
-    id SERIAL NOT NULL,
-    since numeric NOT NULL,
-    
-    tx_id INTEGER REFERENCES transactions(id),
+CREATE TABLE IF NOT EXISTS transaction_inputs(
+    id SERIAL PRIMARY KEY,
+    tx_id INTEGER REFERENCES transaction_digests(id),
     tx_hash bytea NOT NULL,
-    input_index numeric NOT NULL,
+    index INTEGER NOT NULL,
 );
-
-CREATE TABLE IF NOT EXISTS scripts {
-    id SERIAL NOT NULL,
-    script_hash bytea NOT NULL,
+CREATE TABLE IF NOT EXISTS scripts { 
+    id SERIAL PRIMARY KEY,
+    script_hash bytea UNIQUE NOT NULL,
     code_hash bytea NOT NULL,
     hash_type bytea NOT NULL,
     args bytea,
-}
+};
